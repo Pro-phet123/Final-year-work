89d6b0b37948eaecb18bcec3a51b7f25bc065300
Pro-phet123 <olalemiolaoluwakintan@gmail.com>
2023-09-11 15:06:22 +0100

    added @st.cache_data def get_data(filename):     data = pd.read_hdf(filename)     return data to line 62 to 65

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ dataset = st.container()
 visualisations = st.container()
 model_training = st.container()
 
+
 @st.cache_data
 def get_data(filename):
     data = pd.read_csv(filename)
@@ -27,29 +28,16 @@ with dataset:
     st.header('The Online Credit Card Transactions Dataset')
     st.text('I got this dataset from Kaggle. It contains over six million samples and 11 features of online transactions')
     st.subheader('The Datasets First Five Rows(The Head)')
-    
+    online_transactions = get_data(transact.csv)
+    st.write(online_transactions.head())
 
-    file_path = 'C:/Users/User/Documents/Ntel Ola/Project1/transact.csv'
-    if os.path.exists(file_path):
-        # The file exists, proceed with operations on it
-        with open(file_path, 'r') as file:
-        # Perform file operations here
-            online_transactions = get_data(file_path)
-            st.write(online_transactions.head())
-    else:
-        print("File not found at the specified path.")
 
 
 with visualisations:
     st.header('visualisation')
     st.subheader('The Distribution of the Online Credit Card Transactions')
-    file_path = 'C:/Users/User/Documents/Ntel Ola/Project1/transact.csv'
-    if os.path.exists(file_path):
-        # The file exists, proceed with operations on it
-        with open(file_path, 'r') as file:
-        # Perform file operations here
-            payment_type = pd.DataFrame(online_transactions['type'].value_counts())
-            st.bar_chart(payment_type)
+    payment_type = pd.DataFrame(online_transactions['type'].value_counts())
+    st.bar_chart(payment_type)
 
 
 with model_training:
@@ -71,24 +59,22 @@ with model_training:
     input_feature_3 = st.number_input('What is your current account balance?', min_value=0.0, step=1000.0)
     input_feature_4 = st.number_input('What will your account balance be after the transaction?', min_value=0.0, step=1000.0)
 
-model_path = 'C:/Users/User/Documents/Ntel Ola/Project1/autoenconder_mmodel.h5'
-if os.path.exists(model_path):
-        # The file exists, proceed with operations on it
-        with open(model_path, 'r') as file:
-            with h5py.File(model_path, 'r') as model_file:
-                model = tf.keras.models.load_model(model_path)
+@st.cache_data
+def get_data(filename):
+    data = pd.read_hdf(filename)
+    return data
+
+model_path = get_data(autoenconder_mmodel.h5)
+with h5py.File(model_path, 'r') as model_file:    
+    model = tf.keras.models.load_model(model_path)
 
     
 
-                submit = st.button('Predict')
-                if submit:
-                    prediction = model.predict([[input_feature_1, input_feature_2, input_feature_3, input_feature_4]])
-                    threshold = 0.9
-                    if prediction[0][0] > threshold:
-                        st.write('This transaction is **fraudulent.** ')
-                    else:
-                        st.write('This transaction is **non fraudulent.** ')
-#def preprocess_input(input_data):
-        #return input_data
-#if __name__ == "__main__":
-    #main()
+    submit = st.button('Predict')
+    if submit:
+        prediction = model.predict([[input_feature_1, input_feature_2, input_feature_3, input_feature_4]])
+        threshold = 0.9
+        if prediction[0][0] > threshold:
+            st.write('This transaction is **fraudulent.** ')
+        else:
+             st.write('This transaction is **non fraudulent.** ')
